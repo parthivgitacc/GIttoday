03c06494cede5caaaeac187afc8740544ccd1d0f
parthivgitacc <parthieswar17@gmail.com>
2023-02-01 19:50:27 +0530

    test

---
+++ src/org/txs/JavaProg.java
@@ -0,0 +1,31 @@
+package org.txs;
+
+import java.util.Scanner;
+
+public class JavaProg {
+	
+	
+		
+		
+	
+
+	  public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter a number: ");
+	    int number = input.nextInt();
+	    int reverse = 0;
+	    int originalNumber = number;
+	    
+	    while (number != 0) {
+	      int lastDigit = number % 10;
+	      reverse = (reverse * 10) + lastDigit;
+	      number /= 10;
+	    }
+	    
+	    if (originalNumber == reverse) {
+	      System.out.println(originalNumber + " is a reverse of " + reverse);
+	    } else {
+	      System.out.println(originalNumber + " is not a reverse of " + reverse);
+	    }
+	  }
+	}
